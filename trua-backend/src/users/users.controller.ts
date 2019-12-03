bff9da3c0092dcddf3b06bf0df870c53f83450e5
Marta Sušek <m.susek@live.com>
2019-12-03 15:05:31 +0100

    New | Users module.

---
+++ trua-backend/src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Request,UseGuards, Get } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('users')
+export class UsersController {
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get('profile')
+    getProfile(@Request() req) {
+      return req.user;
+    }
+}
