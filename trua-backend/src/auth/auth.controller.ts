37a0a4e66823c8c4a75d5eeebe32ece9ea964368
Marta Sušek <m.susek@live.com>
2019-12-03 15:05:13 +0100

    Formatting

--- trua-backend/src/auth/auth.controller.ts
+++ trua-backend/src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) { }
 
     @UseGuards(AuthGuard('local'))
     @Post('/login')
