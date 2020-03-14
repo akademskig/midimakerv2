12b549cc5eca78f6980918526b6fa7bacfce7907
Marta Sušek <m.susek@live.com>
2020-03-14 15:21:10 +0100

    Add user dropdown

--- frontend-ts/src/layouts/MainLayout.vue
+++ frontend-ts/src/layouts/MainLayout.vue
@@ -3,24 +3,55 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>
-           <q-item>
-          <q-avatar>
-            <q-img src="../assets/robot.svg" width="50px" padding></q-img>
-          </q-avatar>
-        </q-item>
-        </q-toolbar-title>
-        <q-avatar>
-            <q-img src="../assets/robot-head.svg" width="50px" padding></q-img>
-          </q-avatar>
-          <q-item class="items-center">{{currentUser && currentUser.username}}</q-item>
-          <q-item  class="items-center">
-            <q-btn icon-right="logout" label="Logout" flat @click="logout()">
-            </q-btn>
-
+          <q-item>
+            <q-avatar>
+              <q-img src="../assets/robot.svg" width="50px" padding></q-img>
+            </q-avatar>
           </q-item>
+        </q-toolbar-title>
+        <q-btn-dropdown flat :label="currentUser.username">
+          <div class="no-wrap q-pa-none">
+            <div class="items-center">
+              <q-item class="justify-center" bordered>
+                <q-avatar size="72px">
+                  <q-img src="../assets/robot-head.svg" width="50px" padding></q-img>
+                </q-avatar>
+              </q-item>
+              <q-separator/>
+              <q-list >
+                <q-item v-ripple>
+                  <q-item-section class="q-px-none" avatar >
+                    <q-avatar icon="email" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    {{currentUser.email}}
+                  </q-item-section>
+                </q-item>
+               <q-item   clickable v-ripple>
+                  <q-item-section class="q-px-none" avatar >
+                    <q-avatar icon="settings" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    Settings
+                  </q-item-section>
+                </q-item>
+                   <q-item  clickable v-ripple  @click="logout" >
+                  <q-item-section class="q-px-none" avatar >
+                    <q-avatar icon="logout" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    Logout
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </div>
+          </div>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -33,6 +64,12 @@ import Component from 'vue-class-component'
 import { mapGetters, mapActions } from 'vuex'
 @Component({
   name: 'Mainlayout',
+  data () {
+    return {
+      userSelected: null,
+      options: ['Settings', 'Logout']
+    }
+  },
   computed: {
     ...mapGetters({
       currentUser: 'Auth/getCurrentUser'
@@ -42,7 +79,7 @@ import { mapGetters, mapActions } from 'vuex'
     ...mapActions({
       removeUser: 'Auth/removeUser'
     })
-  }
+  },
 })
 export default class MainLayout extends Vue {
   removeUser!: () => void
