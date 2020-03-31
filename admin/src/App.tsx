911bac264598a8b9ec5d555d37c60cd44be576e2
Marta Sušek <m.susek@live.com>
2020-03-31 11:36:00 +0200

    Add auth.provider.

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -8,12 +8,25 @@ import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import DashboardPage from './pages/Dashboard.page';
 import authProvider from './providers/auth.provider';
+import AuthPage from './pages/Auth.page';
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import './App.scss'
 
 const dataProvider = jsonServerProvider('http://localhost:4000');
-const App = () => 
-<Admin dashboard={DashboardPage}dataProvider={dataProvider} authProvider={authProvider}>
-  <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
-  <Resource name="users" list={UserList} icon={UserIcon} />
-</Admin>;
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+  },
+});
+console.log(theme)
+const App = () =>
+    <Admin theme={theme}loginPage={AuthPage} dashboard={DashboardPage} dataProvider={dataProvider}  authProvider={authProvider} >
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+      <Resource name="users" list={UserList} icon={UserIcon} />
+    </Admin>
 
 export default App;
--- admin/src/providers/auth.provider.tsx
+++ admin/src/providers/auth.provider.tsx
@@ -1,29 +1,45 @@
-export default {
-    // called when the user attempts to log in
-    login: ({ username }:{username: string}) => {
-        localStorage.setItem('username', username);
-        // accept all username/password combinations
-        return Promise.resolve();
-    },
-    // called when the user clicks on the logout button
-    logout: () => {
-        localStorage.removeItem('username');
-        return Promise.resolve();
-    },
-    // called when the API returns an error
-    checkError: ({ status }:{status: number}) => {
-        if (status === 401 || status === 403) {
-            localStorage.removeItem('username');
-            return Promise.reject();
+
+const signInUrl = 'http://localhost:4000/auth/signin'
+class AuthProvider {
+  accessToken!: string
+  login = async ({ email, password }: { email: string; password: string }) => {
+    const request = new Request(signInUrl, {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+    });
+    return fetch(request)
+      .then(response => response.json())
+      .then((response) => {
+        if (response.statusCode < 200 || response.statusCode >= 300) {
+          console.error(response)
+          return Promise.reject(response.message)
         }
-        return Promise.resolve();
-    },
-    // called when the user navigates to a new location, to check for authentication
-    checkAuth: () => {
-        return localStorage.getItem('username')
-            ? Promise.resolve()
-            : Promise.reject();
-    },
-    // called when the user navigates to a new location, to check for permissions / roles
-    getPermissions: () => Promise.resolve(),
+        const { access_token } = response
+        localStorage.setItem('access_token', access_token)
+      });
+  }
+  // called when the user clicks on the logout button
+  logout = () => {
+    localStorage.removeItem('access_token');
+    return Promise.resolve();
+  }
+  // called when the API returns an error
+  checkError = ({ status }: { status: number }) => {
+    if (status === 401 || status === 403) {
+      localStorage.removeItem('access_token');
+      return Promise.reject();
+    }
+    return Promise.resolve();
+  }
+  // called when the user navigates to a new location, to check for authentication
+  checkAuth = () => {
+    return localStorage.getItem('access_token')
+      ? Promise.resolve()
+      : Promise.reject();
+  }
+  // called when the user navigates to a new location, to check for permissions / roles
+  getPermissions = () => Promise.resolve()
 };
+
+export default new AuthProvider()
