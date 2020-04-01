73bc77224072588534b38934916660cd06479a2d
Marta Sušek <m.susek@live.com>
2020-04-01 21:13:03 +0200

    Backend: Finish queryBuilder service.

--- trua-backend/src/utils/queryBuilder.service.ts
+++ trua-backend/src/utils/queryBuilder.service.ts
@@ -1,29 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Query } from '@nestjs/common';
 
 @Injectable()
 export class QueryBuilderService {
 
   buildQuery(params, entity) {
-    const paginationQ = this.buildPaginationQuery(params);
-    const searchQ = this.buildSearchQuery(params, entity);
-    return Object.assign({}, paginationQ, searchQ);
+    const paginationQ = this.queryByPaginationParams(params);
+    const searchQ = this.queryBySearchQuery(params, entity);
+    const propQ = this.queryByProp(params);
+    return Object.assign({}, paginationQ, searchQ, propQ);
   }
-    buildPaginationQuery(params) {
+    queryByPaginationParams(params) {
         const { _end, _start, _sort, _order } = params;
-        const order = {
-          [_sort]: _order,
-        };
-        const select = {
-          skip: _start,
-          take: _end,
-        };
-        const query = {
-          order,
-          ...select,
-        };
-        return query;
+        let order = null;
+        let select = null;
+        if (_sort && _order) {
+          order = {
+            [_sort]: _order,
+          };
+        }
+        if (_start && _end) {
+          select = {
+            skip: _start,
+            take: _end,
+          };
+        }
+        return Object.assign({}, order, select);
     }
-    buildSearchQuery(params, entity): {} {
+    queryBySearchQuery(params, entity): {} {
       const {q} = params;
       if (!q) { return []; }
       let fields = [];
@@ -41,6 +44,11 @@ export class QueryBuilderService {
           where = where.concat(' OR ');
         }
       });
+      return {where};
+    }
+    queryByProp(params) {
+      const {id} = params;
+      const where = Object.assign({}, id && {id});
       return {where};
     }
 }
