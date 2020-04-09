8d5ec75ab0702f842339a350369986c50968223d
Marta Sušek <m.susek@live.com>
2020-04-09 20:48:28 +0200

    Backend: Parse validation error messages.

--- trua-backend/src/errors/ValidationErrors.ts
+++ trua-backend/src/errors/ValidationErrors.ts
@@ -1,15 +1,16 @@
 import { ValidationError } from 'class-validator';
 import { BadRequestException } from '@nestjs/common';
+
 class ValidationErrors extends BadRequestException {
-    constructor(validationMessage: ValidationError[]) {
-        const message = validationMessage
+    constructor(validationErrors: ValidationError[]) {
+        const message = validationErrors
         .map(vm => Object.values(vm.constraints)
         .map((item: string) => {
             const mChars = item.split('');
             mChars.splice(0, 1,  mChars[0].toUpperCase());
             return mChars.join('');
         }))
-        .join('');
+        .join('. ');
         super(message);
     }
 }
