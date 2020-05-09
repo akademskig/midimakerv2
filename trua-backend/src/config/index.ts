734db4764f4d9819594e15173b9a6aeabb538507
Marta Sušek <m.susek@live.com>
2020-05-09 15:22:53 +0200

    Remove Sg

--- trua-backend/src/config/index.ts
+++ trua-backend/src/config/index.ts
@@ -1,3 +1,3 @@
 export const PORT = 4000
 export const HOST_URL = `http://localhost:${PORT}`
-export const SG_API_KEY = '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'
+export const SG_API_KEY = ''
