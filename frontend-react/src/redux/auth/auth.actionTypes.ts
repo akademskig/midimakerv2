a25ecf05d367df88d1360b215db71404ccffb1a9
Marta Sušek <m.susek@live.com>
2020-05-11 14:01:19 +0200

    Move auth.actionTypes

---
+++ frontend-react/src/redux/auth/auth.actionTypes.ts
@@ -0,0 +1,8 @@
+export const LOGIN_START = "LOGIN_START"
+export const LOGIN_OK = "LOGIN_OK"
+export const LOGIN_ERROR = "LOGIN_ERROR"
+export const SET_TOKEN = "SET_TOKEN"
+export const REMOVE_TOKEN = "REMOVE_TOKEN"
+export const LOGOUT = "LOGOUT"
+export const UPDATE_USER_START = "UPDATE_USER_START"
+export const UPDATE_USER_OK = "UPDATE_USER_OK"
